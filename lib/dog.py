c3dd3f4b872a69945086e91cb783abab866acf2d
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-09-06 11:40:57 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -20,3 +20,36 @@ def find_by_name_and_breed(session, name, breed):
 
 def update_breed(session, dog, breed):
     pass
+
+
+
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from models import Dog, Base
+
+# SQLITE_URL = 'sqlite:///dogs.db'  
+
+# def create_table(base, engine):
+#     base.metadata.create_all(engine)
+
+# def save(session, dog):
+#     session.add(dog)
+#     session.commit()
+
+# def get_all(session):
+#     return session.query(Dog).all()
+
+# def find_by_name(session, name):
+#     return session.query(Dog).filter_by(name=name).first()
+
+# def find_by_id(session, dog_id):
+#     return session.query(Dog).filter_by(id=dog_id).first()
+
+# def find_by_name_and_breed(session, name, breed):
+#     return session.query(Dog).filter_by(name=name, breed=breed).first()
+
+# def update_breed(session, dog, new_breed):
+#     dog.breed = new_breed
+#     session.commit()
+
